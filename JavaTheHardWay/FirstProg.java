c27109ad588b0cfd608671f56bd99d41226ffca1
Yang Chen <yang.cl7@gmail.com>
2018-12-27 21:24:01 -0700

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+  public static void main( String[] args ) {
+    System.out.println( "My name is Yang Chen, I am determined to learn how to code.");
+    System.out.println( "Today's date is Thursday, December 27, 2018");
+  }
+}
