315fb4bc6be88af429b8b6df7f753837c50864e8
Yang Chen <yang.cl7@gmail.com>
2018-12-28 16:28:20 -0700

    initial commit

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|         Conoco         |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| 2018-12/28     4/17PM  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Gallons:       13.870  |" );
+    System.out.println( "| Price/gallon: $ 2.200  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel Total:  $ 30.51   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
