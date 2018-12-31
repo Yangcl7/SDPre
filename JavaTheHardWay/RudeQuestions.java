e375cd4a0d61c463b7ab005791b686c7280eb559
Yang Chen <yang.cl7@gmail.com>
2018-12-30 17:18:12 -0700

    complete

--- JavaTheHardWay/RudeQuestions.java
+++ JavaTheHardWay/RudeQuestions.java
@@ -4,7 +4,7 @@ public class RudeQuestions {
   public static void main( String[] args ) {
     String name;
     int age;
-    double weight, income, high;
+    double weight, income, height;
 
     Scanner keyboard = new Scanner(System.in);
 
@@ -27,11 +27,11 @@ public class RudeQuestions {
     System.out.print( " Well, thanks for answering my rude questions, " );
     System.out.println( name + ". "  );
 
-    System.out.println( "And one more question, what's your high? ");
-    high = keyboard.nextDouble();
+    System.out.println( "And one more question, what's your height? ");
+    height = keyboard.nextDouble();
 
-    System.out.println(+high +"eh?" );
-    System.out.println(" can you dunk being " +high + "tall?");
+    System.out.println(+height +"eh?" );
+    System.out.println(" can you dunk being " +height + "tall?");
     // Putting a integer when it is expecting a double will blow up I think because integer cant be with desmos.
     // It didn't blow up when enter a integer or double when it is expecting a String, I think it's because String can be all.
     //
