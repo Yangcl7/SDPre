4433779d43179a15c11a458518b6620719c3c9b3
Yang Chen <yang.cl7@gmail.com>
2019-01-03 22:30:15 -0700

    2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+
+  public static void main(String[] args){
+    String name = "Yang Chen";
+    int age = 28;
+    String state = "Colorado";
+
+    System.out.println("name:" + name + " " + "Age:" + age + " " + "State:" +state);
+  }
+}
