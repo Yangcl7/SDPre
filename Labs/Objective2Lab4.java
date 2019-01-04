3b9ff4531d60b3ac755bb86e24b4f752949a5fb0
Yang Chen <yang.cl7@gmail.com>
2019-01-03 22:42:14 -0700

    2Lab4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,13 @@
+public class Objective2Lab4{
+
+  public static void main(String[] args) {
+    double n1, n2, n3, average;
+
+    n1 = 3;
+    n2 = 7;
+    n3 = 599;
+    average = (n1+n2+n3)/3;
+
+    System.out.println(average);
+  }
+}
