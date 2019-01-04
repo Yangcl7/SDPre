6019c0c7b5ffadf126235487b56ac08ad1ddf5d0
Yang Chen <yang.cl7@gmail.com>
2019-01-03 22:37:29 -0700

    2Lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+
+  public static void main(String[] args){
+    String food1, food2, food3;
+
+    food1 = "hamburger";
+    food2 = "sushi";
+    food3 = "steak";
+
+    System.out.println("1." + " " + food1);
+    System.out.println("2." + " " + food2);
+    System.out.println("3." + " " + food3);
+  }
+}
