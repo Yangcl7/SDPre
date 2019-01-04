06da2b6ca4c8bfb856df86e7e501e6d31279024d
Yang Chen <yang.cl7@gmail.com>
2019-01-03 22:16:40 -0700

    1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,8 @@
+public class Objective1Lab3{
+
+  public static void main(String[] args){
+    System.out.println("Name: Yang Chen");
+    System.out.println("Age: 28");
+    System.out.println("State: Colorado");
+  }
+}
