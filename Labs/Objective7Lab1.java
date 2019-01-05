04680d84812a301f614016be8da0fe77783b302a
Yang Chen <yang.cl7@gmail.com>
2019-01-05 15:57:12 -0700

    7Lab1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,10 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+    printName();
+    }
+
+    public static void printName(){ //what is this second public for?
+      System.out.println("Hello Doug");
+
+  }
+}
